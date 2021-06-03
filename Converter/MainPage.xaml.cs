c98350eaab1dbd0d8b63c856eb1ddf388e494b13
pluxury golf <pluxury golf@DESKTOP-SV9M877>
2021-06-03 12:36:14 +0300

    Добавьте файлы проекта.

---
+++ Converter/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Converter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public double kW(double с)
+        {
+            double k = с * 0.735499;
+            return k;
+        }
+        public void Submit_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = kW(Convert.ToDouble(First.Text));
+                double result = a;
+
+                Page1 secondpage = new Page1();
+                Navigation.PushAsync(secondpage);
+                secondpage.Calculator(result);
+            }
+            catch (FormatException)
+            {
+                DisplayAlert("Ошибка", "Ошибка введенных данных!", "Ввести заново");
+            }
+        }
+    }
+
+}
+
+
---
+++ Converter/Page1.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Converter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        public void Calculator(double result)
+        {
+            res.Text = "kW: " + result;
+        }
+    }
+}
